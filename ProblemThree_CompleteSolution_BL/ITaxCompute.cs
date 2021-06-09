512917463e7876a929a018e01047eec75b33e6ea
PS99002489 <71481217+PS99002489@users.noreply.github.com>
2021-06-09 11:58:42 +0530

    Update ITaxCompute.cs

--- ProblemThree_CompleteSolution_BL/ITaxCompute.cs
+++ ProblemThree_CompleteSolution_BL/ITaxCompute.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,3 +11,4 @@ namespace ProblemThree_CompleteSolution_BL
         double TaxOnTotalBill();
     }
 }
+
